455278becc55effcba95309fe38ce2921881bfab
Alekcia-L <alekciawork@gmail.com>
2026-06-10 20:42:51 -0300

    Exercicio Velocidade Media

---
+++ Resolução de Exercicios/06_velocidademedia/index.js
@@ -0,0 +1,8 @@
+const distPercorrida = 500;
+const tempo = 10
+
+const velocidadeMetros = distPercorrida / tempo
+
+const velocidadeKm = velocidadeMetros * 3.6
+
+console.log(`Sua velocidade está em ${velocidadeKm} Km/h`)
