049eb8830e8d57bfce87498f4f774350fb4ffa6b
Alekcia-L <alekciawork@gmail.com>
2026-06-09 21:14:35 -0300

    Calcular a Diatancia entre dois pontos

--- Resolução de Exercicios/05_calcularadistanciaentredoispontos/index.js
+++ Resolução de Exercicios/05_calcularadistanciaentredoispontos/index.js
@@ -1,1 +1,10 @@
+const x1 = 1
+const x2 = 1
+const y1 = 1
+const y2 = 4
 
+//ou pode ser assim: const x1 = 1, x2 = 1, y1 = 1, y2 = 4
+
+const distancia = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+console.log(`A distância entre esses 2 pontos dados é de ${distancia}`)
