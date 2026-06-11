80c9a36e995c5a450383c267091bf31b1fee24b8
Alekcia-L <alekciawork@gmail.com>
2026-06-10 21:07:24 -0300

    Exercicio Casos de COVID

---
+++ Resolução de Exercicios/07_casosdecovid19/index.js
@@ -0,0 +1,7 @@
+const po = 1000
+const x = 4
+const t = 100
+
+const p = po * x ** (t / 7);
+
+console.log(`Após ${t} dias, o total de pessoas infectadas será de ${p}, uma vez que inicialmente existiam ${po} pessoas infectadas.`)
