8f689f8dc949572ae30bd2d8883007102af0d298
Alekcia-L <alekciawork@gmail.com>
2026-06-15 20:37:40 -0300

    Exercicio de Soma dos Angulos Internos de um Poligano

---
+++ Resolução de Exercicios/09_somadossangulosinternosdeumpoligono/index.js
@@ -0,0 +1,9 @@
+const n = 6;
+
+const s = (n - 2) * 180;
+
+console.log(`A soma dos angulos internos é ${s}.`);
+
+const ai = s / n
+
+console.log(`Caso os angulos sejam iguais, cada um deles vale ${ai}.`)
